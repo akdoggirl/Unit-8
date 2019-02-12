958511a85e519c02f2971881ddf33220a5b693a4
akdoggirl <47436034+akdoggirl@users.noreply.github.com>
2019-02-12 09:22:30 -0900

    Update Metaknight.java

--- Metaknight.java
+++ Metaknight.java
@@ -1,4 +1,13 @@
-public class Metaknight
+public class Metaknight extends ClassicCharacters
 {
-    public Metaknight(
+    public Metaknight(int num, boolean a, double s, boolean n, String c)
+    {
+        super(4, false, 4.8, false, "hahaa");
+    }
+    public String toString ()
+    {
+        super.toString();
+        String output = "Metaknight is one of Kirby's plas in many Kirby games";
+        return output;
+    }
 }
